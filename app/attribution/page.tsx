33bb2f586e421df06e630ec05f92f83d6c513ebf
Jesse Johnson <jesseajohnson@pm.me>
2025-04-01 15:55:50 -0400

    update code source url

--- app/attribution/page.tsx
+++ app/attribution/page.tsx
@@ -16,6 +16,7 @@ export default function Attribution() {
         <a
           href="https://www.researchgate.net/publication/2495826_Steering_Behaviors_For_Autonomous_Characters"
           target="_blank"
+          rel="noopener noreferrer"
           className="block max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -32,6 +33,7 @@ export default function Attribution() {
         <a
           href="http://www.kenney.nl"
           target="_blank"
+          rel="noopener noreferrer"
           className="block max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
